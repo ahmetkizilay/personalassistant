329c46375930ffb4947a192fe240073d336e4d82
Ahmet Kizilay <yourstruly@ahmet.kizilay.com>
2010-11-15 12:12:42 +0200

    first commit

---
+++ src/pa/PAConstants.java
@@ -0,0 +1,14 @@
+package pa;
+
+public class PAConstants {
+	public static final int MESSAGE_ERROR = 0;
+	public static final int MESSAGE_INFO = 1;
+	public static final int MESSAGE_DEBUG = 2;
+
+	public static final String COMMAND_ADD = "Add";
+	public static final String COMMAND_SEARCH = "Search";
+	public static final String COMMAND_EDIT = "Edit";
+	public static final String COMMAND_DELETE = "Delete";
+	public static final String COMMAND_HELP = "Help";
+	public static final String COMMAND_UNDEFINED = "Undefined";
+}
