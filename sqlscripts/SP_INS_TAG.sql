5f02cbdce8109c402636aad3a97e135389b121e5
Ahmet Kizilay <yourstruly@ahmet.kizilay.com>
2010-11-16 19:26:25 +0200

    fixed a bug

--- sqlscripts/SP_INS_TAG.sql
+++ sqlscripts/SP_INS_TAG.sql
@@ -1,7 +1,7 @@
 DELIMITER $$
 
 DROP PROCEDURE IF EXISTS `personalassistant_dev`.`SP_INS_TAG`$$
-CREATE PROCEDURE `personalassistant_dev`.`SP_INS_TAG` (
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_INS_TAG`(
 IN in_task_id INT,
 IN in_tag_name VARCHAR(50)
 )
@@ -15,7 +15,7 @@ IF l_tag_exists = 0 THEN
 	INSERT INTO `tags`(`name`) VALUES(in_tag_name);
 END IF;
 
-SELECT MAX(`id`) INTO l_tag_id FROM `tags`;
+SELECT `id` INTO l_tag_id FROM `tags` WHERE `name` = in_tag_name;
 
 SELECT COUNT(*) INTO l_tag_task_exists FROM `tags_tasks` WHERE `tag_id` = l_tag_id AND `task_id` = in_task_id;
 IF l_tag_task_exists = 0 THEN
