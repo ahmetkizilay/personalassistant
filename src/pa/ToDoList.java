bcb3ecda84fdde0816f2e077d93476cadfaba8b0
Ahmet Kizilay <yourstruly@ahmet.kizilay.com>
2010-11-19 15:57:34 +0200

    got rid of the extra execute method.

--- src/pa/ToDoList.java
+++ src/pa/ToDoList.java
@@ -1,27 +1,11 @@
 package pa;
 
-import pa.commands.PATask;
 import pa.commands.PATaskFactory;
 
 public class ToDoList {
 
-	private PATask task;
-	
-	public ToDoList(String[] args) throws Exception{
-		task = getTask(args);
-	}
-
-	private PATask getTask(String[] args) throws Exception{
-		return PATaskFactory.newInstance(args);
-	}
-
-	private void execute(){		
-		task.execute();
-	}
-
 	public static void main(String[] args) throws Exception{
-		ToDoList todoList = new ToDoList(args);
-		todoList.execute();
+		PATaskFactory.newInstance(args);
 	}
 
 }
