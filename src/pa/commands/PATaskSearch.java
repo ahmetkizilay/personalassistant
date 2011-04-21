3b998be2504fc226c01578d5efd36319498bd319
Ahmet Kizilay <yourstruly@ahmet.kizilay.com>
2011-04-22 01:13:22 +0300

    created the template of the search class, waiting to be implemented...

---
+++ src/pa/commands/PATaskSearch.java
@@ -0,0 +1,35 @@
+package pa.commands;
+
+import pa.constants.PACommand;
+
+public class PATaskSearch extends PATask
+{
+
+	public PATaskSearch(String[] args) throws Exception
+	{
+		super(args);
+		this.command = PACommand.SEARCH;
+	}
+
+	@Override
+	public void execute()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void prompt()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void help()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
